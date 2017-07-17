b43db3fffa383fd133f1e07d75b71798b7c3a44c
James Zhao <jamzhao@gmail.com>
2017-07-17 03:43:04 -0700

    Update tmux compat.sh from sstallion dotfiles

--- tmux/tmux/compact.sh
+++ tmux/tmux/compact.sh
@@ -8,49 +8,73 @@
 # to bridge this divide so a common tmux.conf can be used on different
 # systems running different versions without issue.
 
-case `uname -s` in
-Darwin)
-
-    # Setup 'v' to begin selection as in Vim
-    bind-key -Tcopy-mode-vi v send -X begin-selection
-    bind-key -Tcopy-mode-vi y send -X copy-pipe "reattach-to-user-namespace pbcopy"
-
-    # Update default binding of `Enter` to also use copy-pipe
-    unbind -Tcopy-mode-vi Enter
-    bind-key -Tcopy-mode-vi Enter send -X copy-pipe "reattach-to-user-namespace pbcopy"
-
-	;;
-Linux)
-    # Setup 'v' to begin selection as in Vim
-    bind-key -Tcopy-mode-vi v begin-selection
-    bind-key -Tcopy-mode-vi y send -X copy-pipe "reattach-to-user-namespace xclip -sel clip -i"
-
-    # Update default binding of `Enter` to also use copy-pipe
-    # unbind -Tcopy-mode-vi Enter
-    # bind-key -Tcopy-mode-vi Enter send -X copy-pipe "reattach-to-user-namespace xclip -sel clip -i"
-
-	;;
-esac
-
 version=`tmux -V | cut -c 6-`
 has_version() {
-	return `echo $@ | awk "{print (($version >= \$1) == 0)}"`
+    return `echo $@ | awk "{print (($version >= \$1) == 0)}"`
 }
 
+case `uname -s` in
+Darwin)
+    # Reattach to the per-user namespace to access the pasteboard.
+    # For macOS Sierra, specify `--with-wrap-pbcopy-and-pbpaste`
+    # when installing reattach-to-user-namespace via brew(1);
+    # see: https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard,
+    #      http://superuser.com/a/413233.
+    tmux set-option -g default-command "reattach-to-user-namespace -l $SHELL"
+
+    # Key Bindings
+    if has_version 2.4; then
+        tmux bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "pbcopy"
+        tmux bind-key -T copy-mode-vi y     send-keys -X copy-pipe-and-cancel "pbcopy"
+    else
+        tmux bind-key -t vi-copy Enter copy-pipe "pbcopy"
+        tmux bind-key -t vi-copy y     copy-pipe "pbcopy"
+    fi
+    tmux bind-key ] run-shell "pbpaste | tmux load-buffer - && tmux paste-buffer"
+    ;;
+Linux)
+    # Disable terminal clipboard when using gnome-terminal;
+    # see: http://askubuntu.com/a/507215.
+    # tmux set-option -g set-clipboard off
+
+    # Key Bindings
+    if has_version 2.4; then
+        tmux bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "xclip -in -selection clipboard"
+        tmux bind-key -T copy-mode-vi y     send-keys -X copy-pipe-and-cancel "xclip -in -selection clipboard"
+    else
+        tmux bind-key -t vi-copy Enter copy-pipe "xclip -in -selection clipboard"
+        tmux bind-key -t vi-copy y     copy-pipe "xclip -in -selection clipboard"
+    fi
+    tmux bind-key ] run-shell "xclip -out -selection clipboard | tmux load-buffer - && tmux paste-buffer"
+    ;;
+esac
+
+# Key Bindings
+if has_version 2.4; then
+    tmux bind-key -T copy-mode-vi C-j    send-keys -X page-down
+    tmux bind-key -T copy-mode-vi C-k    send-keys -X page-up
+
+    tmux bind-key -T copy-mode-vi Escape send-keys -X cancel
+    tmux bind-key -T copy-mode-vi v      send-keys -X begin-selection
+    tmux bind-key -T copy-mode-vi V      send-keys -X select-line
+    tmux bind-key -T copy-mode-vi r      send-keys -X rectangle-toggle
+else
+    bind-key -t vi-copy C-j    page-down
+    bind-key -t vi-copy C-k    page-up
+
+    bind-key -t vi-copy Escape cancel
+    bind-key -t vi-copy v      begin-selection
+    bind-key -t vi-copy V      select-line
+    bind-key -t vi-copy r      rectangle-toggle
+fi
+
+# Mouse Support
 if has_version 2.1; then
-	# Enable mouse support:
-    set-option -g -q mouse on
-    bind -n WheelUpPane if-shell -F -t = "#{mouse_any_flag}" "send-keys -M" "if -Ft= '#{pane_in_mode}' 'send-keys -M' 'copy-mode -e'"
-
-    # Fix to allow mousewheel/trackpad scrolling in tmux 2.1
-    bind-key -T root WheelUpPane if-shell -F -t = "#{alternate_on}" "send-keys -M" "select-pane -t =; copy-mode -e; send-keys -M"
-    bind-key -T root WheelDownPane if-shell -F -t = "#{alternate_on}" "send-keys -M" "select-pane -t =; send-keys -M"
-
+    tmux set-option -g mouse on
 elif has_version 1.6; then
-	# Enable mouse support:
-	tmux set-option -g mouse-mode on
-	tmux set-option -g mouse-resize-pane on
-	tmux set-option -g mouse-select-pane on
-	tmux set-option -g mouse-select-window on
+    tmux set-option -g mouse-mode on
+    tmux set-option -g mouse-resize-pane on
+    tmux set-option -g mouse-select-pane on
+    tmux set-option -g mouse-select-window on
 fi
 exit 0
