d966dd494c7c9a5b669cf18f56efb145ff8a165b
Hantian James Zhao <hantian@google.com>
2018-05-12 23:59:11 -0400

    Change some tmux config

--- tmux/tmux/compact.sh
+++ tmux/tmux/compact.sh
@@ -46,13 +46,13 @@ Linux)
         tmux bind-key -t vi-copy y     copy-pipe "xclip -in -selection clipboard"
     fi
 
-	if has_version 2.1; then
-		bind-key -T root WheelUpPane if-shell -F -t = "#{alternate_on}" "send-keys -M" "select-pane -t =; copy-mode -e; send-keys -M"
-		bind-key -T root WheelDownPane if-shell -F -t = "#{alternate_on}" "send-keys -M" "select-pane -t =; send-keys -M"
-	else
-		bind-key -t root WheelUpPane if-shell -F -t = "#{alternate_on}" "send-keys -M" "select-pane -t =; copy-mode -e; send-keys -M"
-		bind-key -t root WheelDownPane if-shell -F -t = "#{alternate_on}" "send-keys -M" "select-pane -t =; send-keys -M"
-	fi
+    if has_version 2.1; then
+      tmux bind-key -T root WheelUpPane if-shell -F -t = "#{alternate_on}" "send-keys -M" "select-pane -t =; copy-mode -e; send-keys -M"
+      tmux bind-key -T root WheelDownPane if-shell -F -t = "#{alternate_on}" "send-keys -M" "select-pane -t =; send-keys -M"
+    else
+      tmux bind-key -t root WheelUpPane if-shell -F -t = "#{alternate_on}" "send-keys -M" "select-pane -t =; copy-mode -e; send-keys -M"
+      tmux bind-key -t root WheelDownPane if-shell -F -t = "#{alternate_on}" "send-keys -M" "select-pane -t =; send-keys -M"
+    fi
     tmux bind-key ] run-shell "xclip -out -selection clipboard | tmux load-buffer - && tmux paste-buffer"
     ;;
 esac
