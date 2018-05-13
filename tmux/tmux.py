7c241fbbe3d162384d6b8599ad7cdba67bceb754
James Zhao <jamzhao@gmail.com>
2018-05-13 00:34:00 -0400

    Using tmux plugin manager

--- tmux/tmux.py
+++ tmux/tmux.py
@@ -1,16 +1,44 @@
 from util import util
 
+import subprocess
 import os
 
 LINK_FILES = [('tmux.conf',        '~/.tmux.conf'),
-              ('tmux',             '~/.tmux')]
+              ('dot_tmux',             '~/.tmux')]
+
+def _install_tpm(rootdir, user, package):
+    install_folder = os.path.expanduser("%s/plugins/%s" % (rootdir, package))
+    print "install github at: %s" % install_folder
+    if not os.path.exists(install_folder):
+        cmd_str = "git clone --recursive https://github.com/%s/%s.git %s" % (user, package, install_folder)
+        subprocess.check_call(cmd_str.split())
+    else:
+        old_cwd = os.getcwd()
+        try:
+            os.chdir(install_folder)
+            cmd_str = "git pull"
+            subprocess.check_call(cmd_str.split())
+            cmd_str = "git submodule update --init --recursive"
+            subprocess.check_call(cmd_str.split())
+        finally:
+            os.chdir(old_cwd)
+
+
+def _install():
+    for orig, dest in LINK_FILES:
+        orig = os.path.join(os.path.dirname(__file__), orig)
+        _install_tpm("dot_tmux", "tmux-plugins", "tpm")
+        dest = util.fix_home_path(dest)
+        util.link_file(orig, dest)
 
 
 def install():
-    for orig, dest in LINK_FILES:
-        orig = os.path.join(os.path.dirname(__file__), orig)
-        dest = util.fix_home_path(dest)
-        util.link_file(orig, dest)
+    old_cwd = os.getcwd()
+    try:
+        os.chdir(os.path.dirname(__file__))
+        _install()
+    finally:
+        os.chdir(old_cwd)
 
 
 def uninstall():
